1d151cf5c768f9ff2d11666283efaa53dca07173
root <root@LAPTOP-BPILDDC9.localdomain>
2021-08-22 12:41:50 -0600

    Initial

---
+++ content/firstpost.md
@@ -0,0 +1,17 @@
+Title: My First Pelican Post!
+Slug: my-first-pelican-post
+Date: 09-01-2020
+Author: Joe Shmoe
+
+# This is my first H1 heading!
+
+This is my first paragraph!
+
+## This is my first H2 heading!
+
+- This 
+- is 
+- my
+- first
+- unordered
+- list!
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Lawrence Gray'
+SITENAME = 'Lawrence Gray'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Denver'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('LinkedIn','https://www.linkedin.com/in/larry-gray-phd/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'pelican-themes/bootstrap' 
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
